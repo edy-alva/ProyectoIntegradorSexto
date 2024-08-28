5c409d46f2886c6db4e120209cb96acccec6ef7a
Eddy Trejo <eddyalejo21@gmail.com>
2024-08-28 07:09:45 -0500

    Plantilla Front

---
+++ Codigo/front/TouristTrek/src/app/pages/comercios/comercios.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-comercios',
+  standalone: true,
+  imports: [],
+  templateUrl: './comercios.component.html',
+  styleUrl: './comercios.component.scss'
+})
+export class ComerciosComponent {
+
+}
